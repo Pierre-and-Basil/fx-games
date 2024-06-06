436470b2e7ea2b0452176906fc712230f8583e63
Pierre-and-Basil <pierresameh805@gmail.com>
2024-06-06 09:25:50 +0400

    NEW ACC

--- main.js
+++ main.js
@@ -49,6 +49,10 @@ function auth() {
 		sessionStorage.removeItem('Log');
 		alert("Hi User (Yousef Gihad)")
 		window.location.href = 'login.html';
+        }else if(email == 'taim-3mk' && password == '3mk'){
+		sessionStorage.removeItem('Log');
+		alert("Hi User (Yousef Gihad)")
+		window.location.href = 'login.html';
         }else {
         document.getElementById("error").innerHTML = '<div class="alert alert-danger" role="alert">Wrong Username Or Password!</div>';
         alert("Wrong Username Or Password!");
